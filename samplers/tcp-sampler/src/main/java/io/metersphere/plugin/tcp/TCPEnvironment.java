9bd677df3a42aab44ebd2c3c1e84057cd6111809
fit2-zhao <yong.zhao@fit2cloud.com>
2024-02-19 16:57:39 +0800

    fix: 修复checkbox组件对应数据反序列化错误问题

--- samplers/tcp-sampler/src/main/java/io/metersphere/plugin/tcp/TCPEnvironment.java
+++ samplers/tcp-sampler/src/main/java/io/metersphere/plugin/tcp/TCPEnvironment.java
@@ -2,6 +2,8 @@ package io.metersphere.plugin.tcp;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class TCPEnvironment {
     private String comments;
@@ -10,17 +12,12 @@ public class TCPEnvironment {
     private String port;
     private String connTimeout;
     private String resTimeout;
-    private boolean reUseConnection = true;
-    private boolean noDelay;
-    private boolean closeConnection;
+    private List<String> reUseConnection;
+    private List<String> noDelay;
+    private List<String> closeConnection;
     private String soLinger;
     private String eolByte;
     private String username;
     private String password;
     private String request;
-
-    public String toString() {
-        return "MsEnvironment(comments=" + this.getComments() + ", classname=" + this.getClassname() + ", serverIp=" + this.getServerIp() + ", port=" + this.getPort() + ", connTimeout=" + this.getConnTimeout() + ", resTimeout=" + this.getResTimeout() + ", reUseConnection=" + this.isReUseConnection() + ", noDelay=" + this.isNoDelay() + ", closeConnection=" + this.isCloseConnection() + ", soLinger=" + this.getSoLinger() + ", eolByte=" + this.getEolByte() + ", username=" + this.getUsername() + ", password=" + this.getPassword() + ", request=" + this.getRequest() + ")";
-    }
-
 }
